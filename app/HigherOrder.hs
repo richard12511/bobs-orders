55769650e9f1d22ec4d6a69c4f0eab48b0e7c41b
richard12511 <richardnew12511@gmail.com>
2016-09-15 19:59:57 -0500

    added evenNumberGreaterThan100 to demonstrate function composition

--- app/HigherOrder.hs
+++ app/HigherOrder.hs
@@ -3,3 +3,6 @@ foo f g = f.g
 
 greaterThan100 :: [Int] -> [Int]
 greaterThan100 = filter (>100)
+
+evenNumberGreatherThan100 :: [Int] -> Bool
+evenNumberGreatherThan100 xs = even . length . greaterThan100 $ xs
