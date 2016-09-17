e931a22935cf549199ea7d8e78246fbbc92007e2
richard12511 <richardnew12511@gmail.com>
2016-09-16 19:53:48 -0500

    added foobar function showing non-idomatic haskell

--- app/HigherOrder.hs
+++ app/HigherOrder.hs
@@ -6,3 +6,9 @@ greaterThan100 = filter (>100)
 
 evenNumberGreatherThan100 :: [Int] -> Bool
 evenNumberGreatherThan100 = even . length . greaterThan100
+
+foobar :: [Int] -> Int
+foobar [] = 0
+foobar (x:xs)
+  | x > 3     = (7*x + 2) + foobar xs
+  | otherwise = foobar xs
