cf5f6d6efdc2d86f61ada89987783c1065589e0f
richard12511 <richardnew12511@gmail.com>
2016-09-16 19:57:37 -0500

    added foobar' to show more idomatic haskell

--- app/HigherOrder.hs
+++ app/HigherOrder.hs
@@ -7,8 +7,11 @@ greaterThan100 = filter (>100)
 evenNumberGreatherThan100 :: [Int] -> Bool
 evenNumberGreatherThan100 = even . length . greaterThan100
 
-foobar :: [Int] -> Int
+foobar :: [Integer] -> Integer
 foobar [] = 0
 foobar (x:xs)
   | x > 3     = (7*x + 2) + foobar xs
   | otherwise = foobar xs
+
+foobar' :: [Integer] -> Integer
+foobar' xs =  sum . map (\x -> 7*x + 2) $ (filter (>3) xs)
