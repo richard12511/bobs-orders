c71d8d5b847f28d5415a9a4b3d5137580d6acf76
richard12511 <richardnew12511@gmail.com>
2016-09-19 19:46:05 -0500

    added fun2 function

--- app/Wholemeal.hs
+++ app/Wholemeal.hs
@@ -1,2 +1,8 @@
 fun1 :: [Integer] -> Integer
 fun1 = foldr (*) 1 . map (\x -> x - 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n    = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
