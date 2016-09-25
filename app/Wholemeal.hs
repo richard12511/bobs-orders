3817d3637854757eeb69235aa27b6fbec9fe28be
richard12511 <richardnew12511@gmail.com>
2016-09-25 17:50:02 -0400

    added fun3 funtion which is a refactored point free version of fun2

--- app/Wholemeal.hs
+++ app/Wholemeal.hs
@@ -6,3 +6,5 @@ fun2 1 = 0
 fun2 n
   | even n    = n + fun2 (n `div` 2)
   | otherwise = fun2 (3 * n + 1)
+
+fun3 = sum . filter even . takeWhile (>1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
