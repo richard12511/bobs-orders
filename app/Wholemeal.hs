9bc8114213bb8b28bff06c4c529cf260393e63e5
richard12511 <richardnew12511@gmail.com>
2016-10-03 18:39:55 -0500

    added modules and removed HigherOrder.hs

--- app/Wholemeal.hs
+++ app/Wholemeal.hs
@@ -1,11 +1,14 @@
-fun1 :: [Integer] -> Integer
-fun1 = foldr (*) 1 . map (\x -> x - 2) . filter even
+module Wholemeal
+where
 
-fun2 :: Integer -> Integer
-fun2 1 = 0
-fun2 n
-  | even n    = n + fun2 (n `div` 2)
-  | otherwise = fun2 (3 * n + 1)
+  fun1 :: [Integer] -> Integer
+  fun1 = foldr (*) 1 . map (\x -> x - 2) . filter even
 
-fun3 :: Integer -> Integer
-fun3 = sum . filter even . takeWhile (>1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
+  fun2 :: Integer -> Integer
+  fun2 1 = 0
+  fun2 n
+    | even n    = n + fun2 (n `div` 2)
+    | otherwise = fun2 (3 * n + 1)
+
+  fun3 :: Integer -> Integer
+  fun3 = sum . filter even . takeWhile (>1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
